18e620a4f4c7b86ecfa7b36f29cf052afa06fdf1
MrCoderRaj <rajr127655@gmail.com>
2025-06-05 19:48:26 +0530

    feat: implement deleteOnCloudinary function and enhance error handling

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -24,5 +24,22 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
+const deleteOnCloudinary = async (public_id, resource_type = "image") => {
+    try {
+        if (!public_id) return null;
 
-export { uploadOnCloudinary };
+        // delete file from cloudinary
+
+        const result = await cloudinary.uploader.destroy(public_id, {
+            resource_type: `${resource_type}`
+        });
+
+        return result;
+
+    } catch (error) {
+        console.log('delete on cloudinary failed', error);
+        return error;
+    }
+}
+
+export { uploadOnCloudinary, deleteOnCloudinary };
